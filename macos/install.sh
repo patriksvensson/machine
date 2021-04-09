e331ce88ad943239a9f80348da40f19d68bf64fb
Patrik Svensson <patrik@patriksvensson.se>
2021-04-09 13:00:12 +0200

    macOS: Install Spotify

--- macos/install.sh
+++ macos/install.sh
@@ -6,7 +6,7 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 # oh-my-posh
 brew tap jandedobbeleer/oh-my-posh
 brew install oh-my-posh
-eval "$(oh-my-posh --init --shell zsh --config $(brew --prefix oh-my-posh)/themes/jandedobbeleer.omp.json)"
+eval "$(oh-my-posh --init --shell zsh --config /Users/patrik/Source/github/patriksvensson/machine/config/oh-my-posh.json)"
 source ~/.zshrc
 
 # apps
@@ -16,6 +16,7 @@ brew install --cask slack
 brew install --cask discord
 brew install --cask ripgrep
 brew install --cask marta
+brew install --cask spotify
 
 # rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
